ee17c3fe779369d0a293d5dbb67379b9d3d1b048
Matthew <matthewelento29@gmail.com>
2024-05-25 18:42:33 +0800

    added sql

--- main.go
+++ main.go
@@ -3,46 +3,66 @@ package main
 import (
 	"database/sql"
 	"log"
-	"modernc.org/sqlite"
+	"math/rand"
+	_ "modernc.org/sqlite"
 	"net/http"
 	"strings"
 )
 
 const ABOUT_DIR string = "/whataboutme/"
+const STATIC_DIR string = "web/static/"
+const API_KEY_LENGTH = 36
+
+var api_key = random_key()
 
 func main() {
-	db, err := sql.Open("sqlite", "data/data.db")
+	db, err := sql.Open("sqlite", "./data/data.db")
 	if err != nil {
 		return
 	}
-
 	defer db.Close()
-	sql := `CREATE TABLE analytics (
-          id INTEGER PRIMARY KEY,
-          user_id INT NOT NULL,  
-          event_type TEXT NOT NULL,
-          properties JSON NOT NULL,
-          timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-       );`
+	setup_sql(db)
 
-	_, err := db.Exec(`CREATE TABLE blog`)
+	log.Printf("The key is\n%s", api_key)
 	log.Print("Listening on :8000...")
 
-	http.HandleFunc("GET "+ABOUT_DIR, serve_about)
+	http.HandleFunc("GET /whataboutme", func(w http.ResponseWriter, r *http.Request) { (http.ServeFile(w, r, STATIC_DIR+"hello.html")) })
+	http.HandleFunc("GET "+ABOUT_DIR, serve_assets)
+	http.HandleFunc("GET "+ABOUT_DIR+"blog/{id}", serve_blog)
+	http.HandleFunc("GET "+ABOUT_DIR+"addanewpostyoubingus", serve_new_post)
 	log.Fatal(http.ListenAndServe(":8000", nil))
-
 }
 
-func serve_about(w http.ResponseWriter, r *http.Request) {
-	var page string
-	if string(r.URL.Path) == ABOUT_DIR {
-		log.Print("serving index")
-		page = "web/static/hello.html"
-	} else {
-		log.Print("serving " + page)
-		page = "web/static/" + strings.TrimPrefix(r.URL.Path, ABOUT_DIR)
+func random_key() (key [API_KEY_LENGTH]byte) {
+	for i := 0; i < API_KEY_LENGTH; i++ {
+		key[i] = byte((rand.Int() % 97) + 33)
+	}
+	return key
+}
+
+func setup_sql(db *sql.DB) {
+	table_create := `CREATE TABLE blogs (
+		id INTEGER PRIMARY KEY,
+		thumbnail BLOB,
+		content TEXT NOT NULL,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)`
+	db.Exec(table_create)
+}
+
+func serve_assets(w http.ResponseWriter, r *http.Request) {
+	var page string = STATIC_DIR + "hello.html"
+	if string(r.URL.Path) != ABOUT_DIR {
+		page = STATIC_DIR + strings.TrimPrefix(r.URL.Path, ABOUT_DIR)
+		log.Print("serving " + page + " hell yeah")
 	}
 
 	log.Print("serve")
 	http.ServeFile(w, r, page)
+}
+
+func serve_new_post(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+func serve_blog(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
 }
