809dc04d02cf8e179bf09c2fd07f2a01c94c916c
Matthew <matthewelento29@gmail.com>
2024-05-25 18:42:22 +0800

    yo

---
+++ web/static/js/carousel.js
@@ -0,0 +1,19 @@
+const carousel = document.getElementById('carousel');
+
+
+carousel.addEventListener("scroll", (event) => {
+        // scrollit();
+        console.log("yo");
+});
+carousel.scrollLeft = 500
+
+
+async function scrollit() {
+        carousel.scroll(0,0);
+        while (true) {
+                carousel.scrollBy(1,0);
+                await new Promise((r) => setTimeout(r, 100));
+        }
+}
+scrollit();
+
--- web/static/js/hello.js
+++ web/static/js/hello.js
@@ -5,7 +5,7 @@ const projects = document.getElementById('projects');
 
 // const fonts = ['Ubuntu', 'Noto Sans', 'Serif', 'Verdana', 'FiraCode'];
 const hello_list = ['Hello!', '你好！', 'नमस्ते!', '¡Hola!', 'Bonjour!', 'Привет!', 'Halo!','ハロー!','Hallo!', 'Kamusta!', 'toki a', 'Saluton!'];
-const skills_list = ['My gizmos', '我的小玩意', 'मेरे उपकरण', 'mis artilugios', 'Mes gadgets', 'Мои вещицы', 'alat saya','私のギズモ','Meine Dinge', 'Aking mga gizmos', 'toki a', 'Saluton!', 'ilo mi', 'Miaj aparatoj'];
+const skills_list = ['My gizmos', '我的小玩意', 'मेरे उपकरण', 'mis artilugios', 'Mes gadgets', 'Мои вещицы', 'alat saya','私のギズモ','Meine Dinge', 'Aking mga gizmos', 'ilo mi', 'Miaj aparatoj'];
 const projects_list = ['Projects', '项目', 'परियोजनाओं', 'Proyectos', 'Projets', 'Проекты', 'Proyek', 'プロジェクト', 'Projekte','Mga proyekto', 'pali', 'Projektoj'];
 // const contacts_list = ['Contacts', 'Hola!', 'संपर्क', 'Contactos', 'Contacts', 'Контакты', 'Kontak', '連絡先','Kontakte', 'Mga contact', 'lipu mi', 'Kontaktoj'];
 
@@ -20,7 +20,7 @@ async function changing_hello(hello, hello_list, skills, skills_list, projects, 
                         projects.innerHTML = projects_list[i];
                         // contacts.innerHTML = contacts_list[i];
                         // greeting.style.font_family = fonts[i];
-                        await new Promise((r) => setTimeout(r, 1000));
+                        await new Promise((r) => setTimeout(r, 3000));
                 }
         }
 }
