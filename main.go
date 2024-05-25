893cd2edccb64da9ce8e064f2880f55699951aff
Matthew <matthewelento29@gmail.com>
2024-05-25 18:42:32 +0800

    added the cards

--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
+	"database/sql/driver"
 	"log"
 	"net/http"
 	"strings"
 )
 
-const DIR string = "/whataboutme/"
+const ABOUT_DIR string = "/whataboutme/"
 
 func main() {
-
 	log.Print("Listening on :8000...")
-	http.HandleFunc(DIR+"*", serve_page)
+	http.HandleFunc("GET "+ABOUT_DIR, serve_about)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func serve_page(w http.ResponseWriter, r *http.Request) {
+func serve_about(w http.ResponseWriter, r *http.Request) {
 	var page string
-	if string(r.URL.Path) == DIR {
+	if string(r.URL.Path) == ABOUT_DIR {
 		log.Print("serving index")
 		page = "web/static/hello.html"
-	} else if strings.HasSuffix(r.URL.Path, "/") {
-		http.NotFound(w, r)
-		return
 	} else {
-		page = "web/static/" + strings.TrimPrefix(r.URL.Path, DIR)
 		log.Print("serving " + page)
+		page = "web/static/" + strings.TrimPrefix(r.URL.Path, ABOUT_DIR)
 	}
 
+	log.Print("serve")
 	http.ServeFile(w, r, page)
 }
