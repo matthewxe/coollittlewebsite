4cf593d9e439f23960eca5209e4239cdbae57c27
Matthew <matthewelento29@gmail.com>
2024-05-25 18:42:32 +0800

    rasnteoi

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"database/sql/driver"
+	"database/sql"
 	"log"
+	"modernc.org/sqlite"
 	"net/http"
 	"strings"
 )
@@ -10,9 +11,26 @@ import (
 const ABOUT_DIR string = "/whataboutme/"
 
 func main() {
+	db, err := sql.Open("sqlite", "data/data.db")
+	if err != nil {
+		return
+	}
+
+	defer db.Close()
+	sql := `CREATE TABLE analytics (
+          id INTEGER PRIMARY KEY,
+          user_id INT NOT NULL,  
+          event_type TEXT NOT NULL,
+          properties JSON NOT NULL,
+          timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+       );`
+
+	_, err := db.Exec(`CREATE TABLE blog`)
 	log.Print("Listening on :8000...")
+
 	http.HandleFunc("GET "+ABOUT_DIR, serve_about)
 	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
 
 func serve_about(w http.ResponseWriter, r *http.Request) {
