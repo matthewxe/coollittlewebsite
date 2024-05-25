adc912882a1c4a1c62093f0ccc6cdbf6a6c0f717
Matthew <matthewelento29@gmail.com>
2024-05-25 18:42:22 +0800

    before adding animations

--- web/static/js/carousel.js
+++ web/static/js/carousel.js
@@ -1,19 +1,16 @@
-const carousel = document.getElementById('carousel');
-
+const carousel = document.getElementById("carousel")
 
 carousel.addEventListener("scroll", (event) => {
         // scrollit();
         console.log("yo");
 });
-carousel.scrollLeft = 500
-
+carousel.scrollLeft = 500;
 
 async function scrollit() {
-        carousel.scroll(0,0);
+        carousel.scroll(0, 0);
         while (true) {
-                carousel.scrollBy(1,0);
+                carousel.scrollBy(1, 0);
                 await new Promise((r) => setTimeout(r, 100));
         }
 }
 scrollit();
-
