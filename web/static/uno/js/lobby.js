34f77f0798d39bccf3248843b4c50d9c88720bca
Matthew <matthewelento29@gmail.com>
2024-05-25 18:23:00 +0800

    removed
    Signed-off-by: Matthew <matthewelento29@gmail.com>

--- web/static/uno/js/lobby.js
+++ web/static/uno/js/lobby.js
@@ -22,23 +22,17 @@ document.getElementById("form").onsubmit = function () {
         return false;
 };
 
-if (window["WebSocket"]) {
-        conn = new WebSocket("ws://" + document.location.host + "/uno/ws");
-        conn.onclose = function (evt) {
-                var item = document.createElement("div");
-                item.innerHTML = "<b>Connection closed.</b>";
-                appendLog(item);
-        };
-        conn.onmessage = function (evt) {
-                var messages = evt.data.split("\n");
-                for (var i = 0; i < messages.length; i++) {
-                        var item = document.createElement("div");
-                        item.innerText = messages[i];
-                        appendLog(item);
-                }
-        };
-} else {
+conn = new WebSocket("ws://" + document.location.host + "/uno/ws");
+conn.onclose = function (evt) {
         var item = document.createElement("div");
-        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+        item.innerHTML = "<b>Connection closed.</b>";
         appendLog(item);
-}
+};
+conn.onmessage = function (evt) {
+        var messages = evt.data.split("\n");
+        for (var i = 0; i < messages.length; i++) {
+                var item = document.createElement("div");
+                item.innerText = messages[i];
+                appendLog(item);
+        }
+};
