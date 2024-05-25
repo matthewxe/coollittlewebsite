f743c1cb2d040e7beed57c9f3fd8a9681cddb9bf
Matthew <matthewelento29@gmail.com>
2024-05-25 18:23:01 +0800

    lobby.js
    Signed-off-by: Matthew <matthewelento29@gmail.com>

--- web/static/uno/js/lobby.js
+++ web/static/uno/js/lobby.js
@@ -22,17 +22,23 @@ document.getElementById("form").onsubmit = function () {
         return false;
 };
 
-conn = new WebSocket("wss://" + document.location.host + "/uno/ws");
-conn.onclose = function (evt) {
-        var item = document.createElement("div");
-        item.innerHTML = "<b>Connection closed.</b>";
-        appendLog(item);
-};
-conn.onmessage = function (evt) {
-        var messages = evt.data.split("\n");
-        for (var i = 0; i < messages.length; i++) {
+if (window["WebSocket"]) {
+        conn = new WebSocket("wss://" + document.location.host + "/uno/ws");
+        conn.onclose = function (evt) {
                 var item = document.createElement("div");
-                item.innerText = messages[i];
+                item.innerHTML = "<b>Connection closed.</b>";
                 appendLog(item);
-        }
-};
+        };
+        conn.onmessage = function (evt) {
+                var messages = evt.data.split("\n");
+                for (var i = 0; i < messages.length; i++) {
+                        var item = document.createElement("div");
+                        item.innerText = messages[i];
+                        appendLog(item);
+                }
+        };
+} else {
+        var item = document.createElement("div");
+        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+        appendLog(item);
+}
